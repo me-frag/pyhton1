922d76cb5c30b06e3687082ebee48796a251c10c
me-frag <143004512+me-frag@users.noreply.github.com>
2025-09-13 16:19:57 +0500

    Add files via upload

---
+++ typetask.py
@@ -0,0 +1,25 @@
+name="penguin"
+age="15"
+is_student = True
+weight=38.5
+
+print("name:",name)
+print("Data type of Name is",type(name))
+
+print("Age:",age)
+print("Data type of Age is",type(age))
+
+print("is_student:",is_student)
+print("Data type of is_student",type(is_student))
+
+print("Weight:",weight)
+print("Data type of Weight is",type(weight))
+
+print("\n After Type Casting....")
+age=str(age)
+print(age)
+print("Data Type of age is",type (age))
+weight =int(weight)
+print(weight)
+
+
