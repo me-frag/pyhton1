7c21ca7cd9420da200d096a631425d1c1f530ef9
me-frag <143004512+me-frag@users.noreply.github.com>
2025-09-14 11:03:19 +0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,14 @@
+b1="20 may 2015"
+print("20 may 2015")
+
+b2="1 june 2002"
+print("1 june 2002")
+
+b3="29 november 2009"
+print("29 november 2009") 
+
+b4="30 january 1999"
+print("30 january 1999")
+
+b5="3 december 2011"
+print("3 december 2011")
