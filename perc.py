eb54ca95691e67586ec58cf6078858bb7824be4b
me-frag <143004512+me-frag@users.noreply.github.com>
2025-09-14 16:37:19 +0500

    Add files via upload

---
+++ perc.py
@@ -0,0 +1,12 @@
+print("Enter marks obtained in 4 subject")
+math=int(input("math"))
+english=int(input("english"))
+science=int(input("science"))
+urdu=int(input("urdu"))
+
+sum=math+english+science+urdu
+perc=(sum/400)*100
+print(end="percentage marks")
+print(perc)
+
+
