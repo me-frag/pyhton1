dec5141530d7edd154b569f36061cb4f6ce902f0
me-frag <143004512+me-frag@users.noreply.github.com>
2025-09-20 16:29:38 +0500

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,9 @@
+temperature=int(input("temperature to be checked"))
+print("temperature to be checked:",temperature)
+
+if temperature <20:
+    print("it is cold")
+elif temperature >20:
+    print("it is hot")    
+
+
