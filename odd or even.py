00d174fc65be76385af25b285a84427d30d5f125
me-frag <143004512+me-frag@users.noreply.github.com>
2025-09-20 16:19:38 +0500

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Number to check" ))
+print("Number to be checked :",number)
+
+
+if number%2==0 :
+    print("this is an even number")
+
+else:
+    print("this is an odd number")   
